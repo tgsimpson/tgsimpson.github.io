da1d1d5aac786412a01c08fb2d9da0450badc503
Todd Simpson <toddsimpson@todds-air.attlocal.net>
2018-03-28 11:04:33 -0700

    comment

--- blog_code.js
+++ blog_code.js
@@ -6,9 +6,9 @@ var blog_header =
    '<nav class="navigation">'+
       // '<hr>'+
       '<p>'+
-        '&nbsp&nbsp<a href="./index.html">Home</a> &nbsp&nbsp'+
-        '<a href="./books.html">Books</a> &nbsp&nbsp'+
-        '<a href="./blog.html">Blog</a> &nbsp&nbsp'+
+        '&nbsp&nbsp<a href="./index.html">Home</a> &nbsp/&nbsp'+
+        '<a href="./books.html">Books</a> &nbsp/&nbsp'+
+        '<a href="./blog.html">Blog</a> &nbsp/&nbsp'+
         '<a href="./contact.html">Contact</a> &nbsp&nbsp'+
       '</p>'+
       '<hr>'+
