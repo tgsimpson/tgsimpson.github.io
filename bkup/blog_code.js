fcf98310a2b162c13d36ec7b2cc75ccbdeb4ad8a
Todd Simpson <toddsimpson@todds-air.lan>
2018-09-26 14:26:55 -0700

    comment

---
+++ bkup/blog_code.js
@@ -0,0 +1,20 @@
+
+
+var blog_header = 
+   '<div style="background: linear-gradient(to bottom right,#ddddee, #ddeeff)">'+
+   '&nbsp<h2>&nbsp Todd G. Simpson</h2>'+
+   '<nav class="navigation">'+
+      // '<hr>'+
+      '<p>'+
+        '&nbsp&nbsp<a href="./index.html">Home</a> &nbsp/&nbsp'+
+        '<a href="./blog.html">Blog</a> &nbsp/&nbsp'+
+        '<a href="./books.html">Books</a> &nbsp/&nbsp'+
+        '<a href="./misc.html">Misc</a> &nbsp/&nbsp'+
+        '<a href="./contact.html">Contact</a> &nbsp&nbsp'+
+      '</p>'+
+      '<hr>'+
+    '</nav></div>'; 
+
+
+
+
