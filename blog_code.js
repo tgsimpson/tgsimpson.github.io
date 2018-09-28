78c3cb5d14825c9f8852111bbd3fdbe31640a047
Todd Simpson <toddsimpson@todds-air.lan>
2018-09-28 07:44:43 -0700

    comment

--- blog_code.js
+++ blog_code.js
@@ -1,6 +1,22 @@
 
 
+
 var blog_header = 
+ [
+  '<div style="position: relative;">',
+        '<img src="./Images/Chuck.png" style="width:100%;">',
+        '<div style="position: absolute;bottom: 8px;left: 16px;background-color: white;opacity: 0.65;color: black;padding-left: 20px;padding-right: 20px;text-decoration: none;">',
+            '&nbsp&nbsp<a href="./index.html" style="color:black;text-decoration: none">Home</a> &nbsp/&nbsp',
+            '<a href="./blog.html" style="color:black;text-decoration: none">Blog</a> &nbsp/&nbsp',
+            '<a href="./books.html" style="color:black;text-decoration: none">Books</a> &nbsp/&nbsp',
+            '<a href="./misc.html" style="color:black;text-decoration: none">Misc</a> &nbsp/&nbsp',
+            '<a href="./contact.html" style="color:black;text-decoration: none">Contact</a> &nbsp&nbsp',
+        '</div>',
+        '<div style="position: absolute;top: 8px;right: 16px;"><h2>Todd G. Simpson</h2></div>',
+  '</div>',
+  ].join("")
+
+var blog_header2 = 
    '<div style="background: linear-gradient(to bottom right,#ddddee, #ddeeff)">'+
    '&nbsp<h2>&nbsp Todd G. Simpson</h2>'+
    '<nav class="navigation">'+
