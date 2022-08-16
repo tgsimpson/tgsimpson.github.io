616ce487c200c9f949f789ff65559e32a7f56104
t <t@yonome.com>
2022-08-16 13:37:45 -1000

    comment

--- Maui/map.js
+++ Maui/map.js
@@ -23,8 +23,8 @@ function init2() {
       var queryString = encodeURIComponent('SELECT *');
       console.log("YYYY")
       var query = new google.visualization.Query(
- //        'https://docs.google.com/spreadsheets/d/1Gpi7Xzp1FaHeNFsyAli3iVcG4GOq3Aa4MCng9FoudIM/gviz/tq?sheet=Beaches&headers=1&tq=' + queryString);
-       'https://docs.google.com/spreadsheets/d/1qOAQSGMsyhO8zXUGx1htaqrpDPAQldibMdZpNegMaWg/gviz/tq?sheet=Sheet1&headers=2&tq=' + queryString);
+       'https://docs.google.com/spreadsheets/d/1Gpi7Xzp1FaHeNFsyAli3iVcG4GOq3Aa4MCng9FoudIM/gviz/tq?sheet=Beaches&headers=1&tq=' + queryString);
+      // 'https://docs.google.com/spreadsheets/d/1qOAQSGMsyhO8zXUGx1htaqrpDPAQldibMdZpNegMaWg/gviz/tq?sheet=Sheet1&headers=2&tq=' + queryString);
         console.log("WWWW")
       query.send(handleSSData);
     }
