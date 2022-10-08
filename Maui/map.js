0cfe1652a1f8927d2489f01e0a913b9e6024d8ae
t <t@yonome.com>
2022-10-08 09:23:36 -1000

    comment

--- Maui/map.js
+++ Maui/map.js
@@ -46,6 +46,15 @@ class MauiMap {
                        }
                 } catch {}
        }
+
+
+       this.icons = [
+        // {"id":"Beach", "path":"M 0 0 L 0.10 0.80 Q 0.52.5 0.10, 0.95 0.80 T 0.180 0.80 L 1 1 Z"},
+         {"id":"Trail","path": "M 0 2 L 0.5 1.4 L 1 0.6 L 1.5 0.2 L 2 0 L 2 2 Z"},
+         {"id":"Misc.","path": "M 0 0 L 0 2 L 2 2 L 2 0 L 0 0",},
+//         {"id":"Beach","path": "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",},
+        // {"id":"Trail","path": "M5,3C4.4477,3,4,2.5523,4,2s0.4477-1,1-1s1,0.4477,1,1S5.5523,3,5,3z M12.5,10H10L9,7L8,5.25L9,5l2.3,1l0,0&#xA;&#x9;c0.2761,0.1105,0.5895-0.0239,0.7-0.3S11.9761,5.1105,11.7,5l0,0L9,4H7L5,5L4,6H2.5C2.2239,6,2,6.2239,2,6.5S2.2239,7,2.5,7H5l1-1&#xA;&#x9;l1,2l-2,2v3.5C5,13.7761,5.2239,14,5.5,14S6,13.7761,6,13.5v-3.11L8,9l1,2h3.5c0.2761,0,0.5-0.2239,0.5-0.5S12.7761,10,12.5,10z"},
+       ]
     }
 
     AddAPoint(i) {
@@ -55,13 +64,17 @@ class MauiMap {
                 fillOpacity: 0.8,
                 strokeWeight: 0.4,
                }
+      try {ic.path = this.icons.find(e=>(e.id===AllData[i].Tags[0])).path} catch{} // if there is a custom icon path based on first tag.
       var rs = 1.0 // rescale on zoom
       // color and size based on type
-      try { if (AllData[i].Status.visited)     {rs = 1.4; ic.fillColor = "#00F"}
-            if ("Page" in AllData[i])          {rs = 1.6; ic.fillColor = "#0F0"}
-            if (AllData[i].Status.planning)    {          ic.fillColor = "#FF0"}
-            else if (!AllData[i].Page.proofed) {          ic.strokeWeight = 0.8; ic.strokeColor = "#F80"}
-            if (AllData[i].Tags[0]=="Misc.")   {rs = 1.6; ic.fillColor = "#F0F"}
+      try { if (AllData[i].Status.visited)  
+               if ("Pics" in AllData[i] && AllData[i].Pics.length>0)   
+                                               {rs = 1.4; ic.fillColor = "#00F"}   // visited and Pics
+               else                            {          ic.fillColor = "#44A"}   // visited, but no Pics
+            if ("Page" in AllData[i])          {rs = 1.6; ic.fillColor = "#0F0"}   // has a write up
+            if (AllData[i].Status.planning)    {          ic.fillColor = "#FF0"}   // planning to do more
+            else if (!AllData[i].Page.proofed) {          ic.strokeWeight = 0.8; ic.strokeColor = "#F80"} // write up is good to go
+            if (AllData[i].Tags[0]=="Misc.")   {rs = 1.6; ic.fillColor = "#F0F"}   // miscellaneous
        } catch {}
 
       ic.scale = ic.scale*rs
