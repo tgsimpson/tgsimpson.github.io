16a9625d6c1d4d4caa95726d2aae2122d8b1f228
t <t@yonome.com>
2022-10-17 09:26:30 -1000

    comment

--- Maui/map.js
+++ Maui/map.js
@@ -112,12 +112,12 @@ class MauiMap {
       var rs = 1.0 // size based on type
       try { if (AllData[i].Status.visited)  
                if ("Pics" in AllData[i] && AllData[i].Pics.length>0)   
-                                               {rs = 1.8; ic.fillOpacity = 0.8; ic.fillColor = "#00F"}   // visited and Pics
-               else                            {rs = 1.3; ic.fillOpacity = 0.8; ic.fillColor = "#CCC"}   // visited, but no Pics
+                                               {rs = 1.8; ic.fillOpacity = 0.8; ic.fillColor = "#22F"}   // visited and Pics
+               else                            {rs = 1.3; ic.fillOpacity = 0.8; ic.fillColor = "#66F"}   // visited, but no Pics
             if ("Page" in AllData[i])          {rs = 1.8; ic.fillOpacity = 0.8; ic.fillColor = "#0F0"}   // has a write up
-            if (AllData[i].Status.planning)    {rs = 1.6; ic.fillOpacity = 0.7; ic.fillColor = "#FF0"}   // planning to do more
+            if (AllData[i].Status.planning)    {rs = 1.6; ic.fillOpacity = 0.7; ic.strokeWeight = 0.8; ic.fillColor = "#FF6"; ic.strokeColor = "#000"}   // planning to do more
             else if (AllData[i].Page.proofed)  {rs = 1.8; ic.fillOpacity = 0.8; ic.strokeWeight = 0.8; ic.strokeColor = "#000"} // write up is good to go
-            if (AllData[i].Tags[0]=="Misc.")   {rs = 1.5; ic.fillOpacity = 0.6; ic.fillColor = "#F0F"}   // miscellaneous
+            if (AllData[i].Tags[0]=="Misc.")   {rs = 1.5; ic.fillOpacity = 0.6; ic.fillColor = "#F3F"}   // miscellaneous
        } catch {}
        // custom icon in AllData?
        try { if ("Icon" in AllData[i]) {
